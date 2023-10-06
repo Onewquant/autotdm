d7f07a718dabb5c931b8832ed3a28058746e899c
onew <seunghwan0216@gmail.com>
2023-10-06 16:36:11 +0900

    mc

--- practice.py
+++ practice.py
@@ -17,3 +17,38 @@ collection_df[['id']].merge(done_df[['id','name']], how='left',on=['id']).to_csv
 
 collection_df['id'].iloc[0]
 done_df['id'].iloc[0]
+
+
+
+tdf = pd.read_excel('D:/autotdm/two_departments.xlsx')
+tdf.columns
+ndf = list()
+# for inx, row in tdf.iterrows(): break
+for inx, row in tdf.iterrows():
+    pnum = int(row['name'][-1])
+    pname = row['name'][:-1]
+
+    if pnum==2:
+        new_dict = dict()
+        for c in ('id','name','sex','age','height', 'weight','tdm_date'):
+            if c=='name':
+                new_dict[c] = pname
+            else:
+                new_dict[c] = row[c]
+        for c in ('Dose per Adm (mg)','Dosing Interval (h)','Vd (L/kg)','CL (ml/min/kg)','total CL (L/hr)', 'Vd steady state(L)', 'trough concentration', 'AUC (mg*h/L)'):
+            new_dict[c+'_2point'] = row[c]
+        prev_pname = pname
+    if pnum==1:
+        if pname==prev_pname:
+            for c in ('Vd (L/kg)','CL (ml/min/kg)','total CL (L/hr)', 'Vd steady state(L)', 'trough concentration', 'Daily Dose (mg)', 'AUC (mg*h/L)'):
+                new_dict[c+'_1point'] = row[c]
+            ndf.append(new_dict)
+        else:
+            print('Patient 이름이 일치하지 않습니다.')
+            raise ValueError
+
+ndf = pd.DataFrame(ndf)
+ndf['AUC2'] = ndf['AUC (mg*h/L)_2point'].copy()
+ndf['AUC1'] = ndf['AUC (mg*h/L)_1point'].copy()
+ndf = ndf.drop(labels=['AUC (mg*h/L)_2point','AUC (mg*h/L)_1point'], axis=1)
+ndf.to_csv('D:/autotdm/two_departments_수정.csv', header=True, encoding='utf-8-sig', index=False)
